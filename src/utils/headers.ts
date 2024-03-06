c62c9d7623798cb7054298b08171d7f1b10d6979
Ryan Martin <eqpoqpe@gmail.com>
2024-03-06 14:53:24 +0800

    for fun

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -3,10 +3,10 @@ import { defu } from "defu";
 
 export function mergeOptions4AuthHeaders(
   sessionKey: string,
-  options: AxiosRequestConfig | undefined
+  options: AxiosRequestConfig | undefined,
 ) {
   return defu(
     { headers: { Authorization: `${sessionKey}` } } as AxiosRequestConfig,
-    options
+    options,
   );
 }
